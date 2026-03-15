295adbe0dd9f075797bca6952aa9821bac341378
Mayank kumar <mayankkumarsharma244@gmail.com>
2026-03-15 15:23:23 +0530

    Add files via upload

---
+++ listing.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        filename: String,
+        url: {
+        type: String,
+        default: 
+            "https://unsplash.com/photos/cozy-living-room-with-rocking-chairs-and-bookshelf-yX14UVs6dWE",
+        set : (v) => 
+            v === "" 
+                ?"https://unsplash.com/photos/cozy-living-room-with-rocking-chairs-and-bookshelf-yX14UVs6dWE"
+                : v,
+        },
+    },
+    price: Number,
+    location: String, 
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
